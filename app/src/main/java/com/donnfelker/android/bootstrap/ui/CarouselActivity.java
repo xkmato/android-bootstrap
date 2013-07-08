25f75b53b8dea618fe44e26c6671be0bac880763
Donn Felker <donn@donnfelker.com>
2013-07-07 23:00:45 -0700

    Adding sliding menu from net.simonvt.menudrawer

--- app/src/main/java/com/donnfelker/android/bootstrap/ui/CarouselActivity.java
+++ app/src/main/java/com/donnfelker/android/bootstrap/ui/CarouselActivity.java
@@ -2,31 +2,30 @@
 
 package com.donnfelker.android.bootstrap.ui;
 
-import static android.content.Intent.FLAG_ACTIVITY_CLEAR_TOP;
-import static android.content.Intent.FLAG_ACTIVITY_SINGLE_TOP;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
+import android.view.View;
 
-import com.actionbarsherlock.view.Menu;
 import com.actionbarsherlock.view.MenuItem;
 import com.actionbarsherlock.view.Window;
-import com.donnfelker.android.bootstrap.BootstrapApplication;
 import com.donnfelker.android.bootstrap.R;
-import com.donnfelker.android.bootstrap.R.id;
-
 import com.viewpagerindicator.TitlePageIndicator;
 
 import butterknife.InjectView;
 import butterknife.Views;
+import net.simonvt.menudrawer.MenuDrawer;
+
 
 /**
  * Activity to view the carousel and view pager indicator with fragments.
  */
 public class CarouselActivity extends BootstrapFragmentActivity {
 
-    @InjectView(id.tpi_header) TitlePageIndicator indicator;
-    @InjectView(id.vp_pages) ViewPager pager;
+    @InjectView(R.id.tpi_header) TitlePageIndicator indicator;
+    @InjectView(R.id.vp_pages) ViewPager pager;
+
+    private MenuDrawer menuDrawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,23 +33,59 @@ public class CarouselActivity extends BootstrapFragmentActivity {
         requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
 
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.carousel_view);
+
+        // Set up navigation drawer
+        menuDrawer = MenuDrawer.attach(this);
+        menuDrawer.setMenuView(R.layout.navigation_drawer);
+        menuDrawer.setContentView(R.layout.carousel_view);
+        menuDrawer.setSlideDrawable(R.drawable.ic_drawer);
+        menuDrawer.setDrawerIndicatorEnabled(true);
+
+        Views.inject(this);
 
         pager.setAdapter(new BootstrapPagerAdapter(getResources(), getSupportFragmentManager()));
 
         indicator.setViewPager(pager);
         pager.setCurrentItem(1);
+
+        setNavListeners();
+    }
+
+    private void setNavListeners() {
+
+        menuDrawer.findViewById(R.id.home).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                menuDrawer.toggleMenu();
+            }
+        });
+
+        menuDrawer.findViewById(R.id.timer).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                menuDrawer.toggleMenu();
+                navigateToTimer();
+            }
+        });
+
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch(item.getItemId()) {
-            case id.timer:
-                final Intent i = new Intent(this, BootstrapTimerActivity.class);
-                startActivity(i);
+            case android.R.id.home:
+                menuDrawer.toggleMenu();
+                return true;
+            case R.id.timer:
+                navigateToTimer();
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
         }
+    }
+
+    private void navigateToTimer() {
+        final Intent i = new Intent(this, BootstrapTimerActivity.class);
+        startActivity(i);
     }
 }
